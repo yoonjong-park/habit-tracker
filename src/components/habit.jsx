cd004e189e59f30b43f607a1c952468c715b511e
yoonjong.park <yoonjongpark@gmail.com>
2020-12-20 21:03:24 +0900

    Update : completed habit component
    ADD plus, minus, delete

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-    render() {
-        return (
-        <li className="habit">
-            <span className="habit-name">Reading</span>
-            <span className="habit-count">8</span>
-            <button className="habit-button habit-increase">
-              <i className="fas fa-plus-square"></i>
-            </button>
-            <button className="habit-button habit-decrease">
-              <i className="fas fa-minus-square"></i>
-            </button>
-            <button className="habit-button habit-delete">
-              <i className="fas fa-trash"></i>
-            </button>
-        </li>
-        );
-    }
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
+  };
+
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+
+  render() {
+    const { name, count } = this.props.habit;
+    return (
+      <li className='habit'>
+        <span className='habit-name'>{name}</span>
+        <span className='habit-count'>{count}</span>
+        <button className='habit-button habit-increase' onClick={this.handleIncrement}>
+          <i className='fas fa-plus-square'></i>
+        </button>
+        <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
+          <i className='fas fa-minus-square'></i>
+        </button>
+        <button className='habit-button habit-delete' onClick={this.handleDelete}>
+          <i className='fas fa-trash'></i>
+        </button>
+      </li>
+    );
+  }
 }
 
 export default Habit;
---
+++ src/components/habits.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import Habit from './habit';
+
+class Habits extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Running', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits }); /* key : value -> 1개로 통합 가능*/
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id); /* click한 거 빼고 저장 */
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.state.habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+          /***  Key는 항상 제공 되어야 함. habit은 props를 의미함. {habit} 부분은habits object의 1개 배열의 아이템을 의미함.***/
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Habits;
